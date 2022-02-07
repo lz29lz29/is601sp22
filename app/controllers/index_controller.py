6f286fe2320a32f1512cb30a5f45db4b3ef4215d
ming <ming@mings-MacBook-Pro.local>
2022-02-06 21:16:41 -0500

    Updated Name

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "kwilliam"
+        name = "Liming Zhou"
         return render_template('index.html', name=name)
